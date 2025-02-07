f411546e47b089af723effe8e436144ee24965c5
Cipher Professor <mohsinmanzoor1913@gmail.com>
2025-02-08 00:28:38 +0530

    Page fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,11 @@
 "use client";
 
-import { HeroSection } from "@/components/home/HeroSection/HeroSection";
 import HomePage from "./(routes)/home/page";
-import { BackgroundBeams } from "@/components/ui/AcUI/BackgroundBeams/background-beams";
-// import HeroSection from "@/components/home/HeroSection/HeroSection";
-// import HeroUIJustTabs from "@/components/ui/HeroUI/Tabs/JustTabs";
-
-
-
 
 export default function Home() {
   return (
-   
     <>
-      {/* <div className="flex flex-col items-center justify-center m-20 w-full p-20"> */}
-        {/* <HeroUIJustTabs /> */}
-       
-       < HomePage />
-      {/* </div> */}
+      <HomePage />
     </>
   );
 }
